919a92404cc0ab26da3b5f2abab80bf348639f02
PANCE <aswarhamid12@gmail.com>
2026-09-12 22:27:31 +0800

    test

---
+++ aritmatika/aritmatika.go
@@ -0,0 +1,18 @@
+// operator arimatika
+// + -> a + b
+// - -> a-b
+// * -> a * b
+// / -> a / b
+// % -> a % b
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int = 10, 3
+	fmt.Println("nilai a + b adalah ", a+b)
+	fmt.Println("nilai a - b adalah ", a-b)
+	fmt.Println("nilai a * b adalah ", a*b)
+	fmt.Println("nilai a / b adalah ", a/b)
+	fmt.Println("nilai a % b adalah ", a%b)
+}
